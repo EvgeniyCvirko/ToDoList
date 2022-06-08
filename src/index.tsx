212596b793edf67f566f0f192e5c91bb0faa7d49
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2022-06-08 23:10:01 +0300

    refactor for React 18

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,21 @@ import {AppWithReducer} from "./AppWithReducer";
 import {AppWithRedux} from "./AppWithRedux";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
+// @ts-ignore
+import {createRoot} from "react-dom/client";
 
-
-ReactDOM.render(
+/*ReactDOM.render(
   <Provider store={store}>
     <AppWithRedux />
   </Provider>,
   document.getElementById('root')
-);
+);*/
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<Provider store={store}>
+    <AppWithRedux/>
+</Provider>,);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
